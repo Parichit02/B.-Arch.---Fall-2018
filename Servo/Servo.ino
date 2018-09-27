c3189c33fad6b62e2310f0dadce498c4bf50b357
Parichit Kumar <parichitcdrr@gmail.com>
2018-09-27 11:44:25 -0400

    Servo Files Added

---
+++ Servo/Servo.ino
@@ -0,0 +1,23 @@
+#include <Servo.h> //In-built servo library
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(27);  // attaches the servo on pin 27 to the servo object
+}
+
+void loop() {
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(1);                       // generate a delay between each increment
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(1);                       // generate a delay between each increment
+  }
+  delay(1000);
+}
